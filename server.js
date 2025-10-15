ca9157f179b6e61d9444ccf04629fa850dead536
Marnitz Malan <malanmarnitz@gmail.com>
2025-10-15 10:29:08 +0200

    server listhen to 0.0.0.0

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { createApp } from "./src/app.js";
 const start = async () => {
   const app = await createApp();
   const port = process.env.PORT || 5000;
-  await app.listen({ port });
+  await app.listen({ port, host: '0.0.0.0' });
   const publicUrl = process.env.RAILWAY_PUBLIC_DOMAIN || process.env.PUBLIC_URL || null;
   if (publicUrl) {
     console.log(`🚀 Server running on https://${publicUrl}`);
